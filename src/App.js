acd6f7263cbd8eb747ceea6c66fcc8a4ddd9662e
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-04-25 14:07:57 +0200

    Display values from an array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MyFirstComponent from './components/myFirstComponent'
 import Add from './components/Add'
 import Header from './components/Header'
+import MappingArray from './components/MappingArray'
 
 const App = () => (
     <div>
@@ -9,6 +10,26 @@ const App = () => (
         <Add numberA={3} numberB={5} />
         <Add numberA={13} numberB={75} />
         <Header text={'Ala ma kota'} isRed={false}/>
+
+        <hr />
+
+        <p>null: {null}</p>
+        <p>undefined: {undefined}</p>
+        <p>true: {true}</p>
+        <p>false: {false}</p>
+        <p>string 'Ala ma kota': {'Ala ma kota'}</p>
+        <p>number 23: {23}</p>
+        
+        <p>array [1, 2, 3]: {[1, 2, 3]}</p>
+        <p>array ['Ala', 'Ola', 'Ela']: {['Ala', 'Ola', 'Ela']}</p>
+        <div>array with JSX: {[<b key={1}>BOLD</b>, <h1 key={2}>H1</h1>]}</div>
+
+        {/* <p>object: {{}}</p> */}
+
+        <hr />
+
+        <MappingArray />
+        
     </div>
 )
 
