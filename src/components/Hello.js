a23cea5bd468ce433879afdb61685b61ebf7510e
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-04-26 12:22:09 +0200

    Display Hello using props.match.params.name nested with withRouter

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 
-const HelloNested = (props) => (
+const HelloNested = withRouter((props) => (
   <h1>Hello {props.match.params.name}!</h1>
-)
+))
+
+const HelloNestedWithRouter = withRouter(HelloNested)
 
 const Hello = (props) => (
-  <HelloNested {...props} />
+  <HelloNestedWithRouter />
 )
 
 export default Hello
